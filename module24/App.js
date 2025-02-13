9c9118f02b3552811b2b2ef7a808d6a7f2ef86e5
OmarBahtijari <bahtijariomar49@gmail.com>
2025-02-13 20:47:43 +0100

    module25

---
+++ module24/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {createBottomNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import RandomDog from './screens/RandomDog';
+
+const Tab = createBottomNavigator();
+
+export default function App() {
+  return (
+   <NavigationContainer>
+    <Tab.Screen
+        name='AllDogs' component={RandomDog}
+    />
+   </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ module25/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Fox from './screens/Fox';
+import Duck from './screens/Duck';
+
+export default function App() {
+  return (
+    <Fox/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
